5ff584df72c5f8cfa3472fcf6de3cd4c1fbc90e0
Julian <ozon.msr@gmail.com>
2020-06-23 01:11:35 +0300

    final

--- task_2.py
+++ task_2.py
@@ -1,9 +1,13 @@
 last = 0
 now = 1
+y = 0
 while now < 4000000:
     sum = last + now
     x = now % 2
     if x == 0: 
-        print(now)
+        print('чётное число Фибоначчи:', now)
+        y = y + now
+        print('текущая сумма:', y)
     last = now
     now = sum
+print('ОТВЕТ:', y)
