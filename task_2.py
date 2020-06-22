3d76aded148e2f6657e542083da9dcadf58b40e4
Julian <ozon.msr@gmail.com>
2020-06-23 00:45:52 +0300

    showed_even_Fib_numbers

--- task_2.py
+++ task_2.py
@@ -3,8 +3,7 @@ now = 1
 while now < 4000000:
     sum = last + now
     x = now % 2
-    if x == 0:
-        even = x
-        print(even)
+    if x == 0: 
+        print(now)
     last = now
     now = sum
