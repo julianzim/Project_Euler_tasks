3fe0626e9c0e842c49725e17d4b619631e936294
Julian <ozon.msr@gmail.com>
2020-06-23 19:17:06 +0300

    final?

--- task_3.py
+++ task_3.py
@@ -1,6 +1,14 @@
-a = 15
-lst = []
-for i in range(1, a+1):
-    if a % i == 0:
-        lst.append(i)
-print(lst)
+a = 13195
+deviders = []
+prime_dev = []
+for k in range(1, a+1):
+    if a % k == 0:
+        deviders.append(k)
+#print('Список делителей числа', a, ':', deviders)
+for i in deviders:
+    for j in range(2, i):
+        if i % j == 0:
+            break
+    else:
+        prime_dev.append(i)
+print('Из них простые:', prime_dev)
