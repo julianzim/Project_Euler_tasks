b6fd6ada1f37e5d61da6b1a4fdf41a159517884d
Julian <ozon.msr@gmail.com>
2020-06-23 21:20:33 +0300

    with sqrt

--- task_3.py
+++ task_3.py
@@ -1,4 +1,5 @@
-a = 13195
+from math import sqrt
+a = 1314953434
 deviders = []
 prime_dev = []
 for k in range(1, a+1):
@@ -7,6 +8,9 @@ for k in range(1, a+1):
 #print('Список делителей числа', a, ':', deviders)
 for i in deviders:
     for j in range(2, i):
+        if j > int((sqrt(i)) + 1):
+            prime_dev.append(i)
+            break
         if i % j == 0:
             break
     else:
